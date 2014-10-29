a57b1e40274dbab281877195d397f9be45dcd4f3
Shomari <Shomari.Ewing@gmail.com>
2014-10-29 15:33:31 -0500

    forgot to save

--- db/migrate/20141029201503_create_votes.rb
+++ db/migrate/20141029201503_create_votes.rb
@@ -1,6 +1,9 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
+      t.integer :voteable_id
+      t.string  :voteable_type
+      t.boolean :upvote?
 
       t.timestamps
     end
