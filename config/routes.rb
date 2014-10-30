0a96eb334ca217145f099114f17ea9c7bd638c3c
Raphael <boo.rho@gmail.com>
2014-10-30 11:15:04 -0500

    indentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
   resources :categories, only: [:index] do
-	  resources :places, except: [:destroy] do
-	    resources :votes
-	    resources :reviews do
-	      resources :votes
-	    end
-	  end
-	end
+    resources :places, except: [:destroy] do
+      resources :votes
+      resources :reviews do
+        resources :votes
+      end
+    end
+  end
 
 
 
